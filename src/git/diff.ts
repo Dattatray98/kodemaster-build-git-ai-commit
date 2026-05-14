126e04a7680e98901827e86730a6beceea5c3531
jojewar Dattatray <jojewardattatray@gmail.com>
2026-05-14 21:02:13 +0530

    feat: implement git diff reading

---
+++ src/git/diff.ts
@@ -0,0 +1,16 @@
+import {exec} from "child_process";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+export async function getStagedDiff(): Promise<string> {
+    try{
+
+        const {stdout} = await execAsync('git diff --staged');
+        return stdout;
+    }catch(error){
+        console.error('Error reading git diff : ', error);
+        return '';
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { Command } from 'commander';
 import chalk from 'chalk';
 import process from 'process';
+import { getStagedDiff } from './git/diff';
 
 const program = new Command();
 
@@ -15,17 +16,20 @@ program
 program
   .command("hello")
   .description("says hello")
-  .description("says hello")
   .action(()=>{
     console.log(chalk.green('Hello world'));
     console.log(chalk.gray('This is styled text'));
   });
-// TODO: Uncomment the code below to pass the first stage
-// program
-//   .command('hello')
-//   .description('Test command')
-//   .action(() => {
-//     console.log(chalk.green('Hello from git-ai-commit!'));
-//   });
+
+
+program
+  .command("diff")
+  .description("shows git diff")
+  .action(async ()=>{
+    const stdout = await getStagedDiff();
+    console.log(stdout);
+  });
+
+
 
 program.parse(process.argv);
