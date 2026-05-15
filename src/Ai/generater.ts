2179daccde5770abe1fd07063046cf85c7bd63cd
jojewar Dattatray <jojewardattatray@gmail.com>
2026-05-15 10:11:43 +0530

    feat: implement prompt generator logic

--- src/Ai/generater.ts
+++ src/Ai/generater.ts
@@ -1,16 +1,26 @@
 import { openai } from "./openai";
+import ollama from "ollama";
 
-const SYSTEM_PROMPT = 'You are a specialized commit message generator. Write a concise, conventional commit message based on the provided diff.';
+const SYSTEM_PROMPT = `You are a specialized commit message generator. Write a concise, conventional commit message based on the provided diff. without any single extra character, and also don't tell what you did, i want only the commit message line only. `;
 
 export const generateCommitMessage = async (diff: string): Promise<string> =>{
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4.1-mini",
-        messages: [
-            {role: "system", content:SYSTEM_PROMPT},
-            {role: "user", content:diff}
-        ],
-        max_tokens:200
-    });
+    try{
+        const completion = await openai.chat.completions.create({
+            model: "gpt-4.1-mini",
+            messages: [
+                {role: "system", content:SYSTEM_PROMPT},
+                {role: "user", content:diff}
+            ],
+            max_tokens:200
+        });
+    
+        return completion.choices[0].message.content || 'Error: No message generated';
+    }catch(error){
+        const completion = await ollama.generate({
+            model:"llama3",
+            prompt: "system " + SYSTEM_PROMPT + ",user : " + diff,
+        })
 
-    return completion.choices[0].message.content || 'Error: No message generated';
+        return completion.response;
+    }
 }
