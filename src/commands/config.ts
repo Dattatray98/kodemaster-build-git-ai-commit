1eadbaa15e7a13f9984d4c2bebc2a318c620cf2b
jojewar Dattatray <jojewardattatray@gmail.com>
2026-05-20 01:21:12 +0530

    feat: add AI provider name and model name configuration commands

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,7 +1,8 @@
 import { Command } from "commander";
 import { setupConfig } from "../config/setup";
+import { loadConfig } from "../config/config";
 
-const configCommand = new Command("config")
+export const configCommand = new Command("config")
     .description("Configure Navix AI provider and model")
     .option("--provider <provider>", "AI provider")
     .option("--model <model>", "Model name")
@@ -9,4 +10,15 @@ const configCommand = new Command("config")
         const { provider, model } = options;
         await setupConfig(provider, model);
     })
-export default configCommand;
+
+
+export const checkConfigCommand = new Command("check-config")
+    .description("check the AI provider name and model name")
+    .action(async () => {
+        const config = await loadConfig();
+        if (!config) {
+            console.log("config is missing!");
+        }
+        console.log(config)
+    })
+
--- src/core/index.ts
+++ src/core/index.ts
@@ -4,13 +4,14 @@ import { Command } from 'commander';
 import chalk from 'chalk';
 import { getStagedDiff } from '../git_tools/diff';
 import { filterChanges, parseDiff } from '../git_tools/parser';
-import configCommand from '../commands/config';
+import { checkConfigCommand, configCommand } from '../commands/config';
 import { commitCommand } from '../commands/commit';
 
 
 const program = new Command();
 
 program.addCommand(configCommand);
+program.addCommand(checkConfigCommand);
 
 program
   .name('navix')
